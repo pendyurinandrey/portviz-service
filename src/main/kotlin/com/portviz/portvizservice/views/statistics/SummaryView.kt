e4dffca16a9877e1fe4fffbcffe9fdc6ff3e764c
pendyurinandrey <pendyurinandrey@gmail.com>
2023-11-12 23:11:30 +0500

    Improved bar chart

--- src/main/kotlin/com/portviz/portvizservice/views/statistics/SummaryView.kt
+++ src/main/kotlin/com/portviz/portvizservice/views/statistics/SummaryView.kt
@@ -8,6 +8,8 @@ import jakarta.inject.Named
 import org.primefaces.model.charts.ChartData
 import org.primefaces.model.charts.bar.BarChartDataSet
 import org.primefaces.model.charts.bar.BarChartModel
+import org.primefaces.model.charts.bar.BarChartOptions
+import org.primefaces.model.charts.optionconfig.title.Title
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 
@@ -18,28 +20,40 @@ class SummaryView @Autowired constructor(
     @Qualifier("demoPortvizStatisticsService") private val demoPortvizStatisticsService: PortvizStatisticsService
 ) {
 
-    val incomeOutcomeBarChart get(): BarChartModel {
-        var statEntries: List<IncomeOutcomeEntry>? = null
-        if (SecurityUtils.isDemoAccount()) {
-            statEntries = demoPortvizStatisticsService.getIncomeOutcomeStatistics()
+    val incomeOutcomeBarChart
+        get(): BarChartModel {
+            var statEntries: List<IncomeOutcomeEntry>? = null
+            if (SecurityUtils.isDemoAccount()) {
+                statEntries = demoPortvizStatisticsService.getIncomeOutcomeStatistics()
+            }
+            val barChart = BarChartModel()
+            val data = ChartData()
+            val labels = statEntries?.map { it.date.toString() }
+            data.labels = labels
+
+            val incomeDataSet = BarChartDataSet()
+            val incomeValues = statEntries?.map(IncomeOutcomeEntry::income)
+            incomeDataSet.data = incomeValues
+            incomeDataSet.label = "Income, \$"
+            incomeDataSet.backgroundColor = "#097969"
+            data.addChartDataSet(incomeDataSet)
+
+            val outcomeDataSet = BarChartDataSet()
+            val outcomeValues = statEntries?.map(IncomeOutcomeEntry::outcome)
+            outcomeDataSet.data = outcomeValues
+            outcomeDataSet.label = "Outcome, \$"
+            outcomeDataSet.backgroundColor = "#AA4A44"
+            data.addChartDataSet(outcomeDataSet)
+
+            val opts = BarChartOptions()
+            val title = Title()
+            title.isDisplay = true
+            title.text = "Income and outcome, \$/year"
+            opts.title = title
+
+            barChart.data = data
+            barChart.options = opts
+
+            return barChart
         }
-        val barChart = BarChartModel()
-        val data = ChartData()
-        val labels = statEntries?.map { it.date.toString() }
-        data.labels = labels
-
-        val incomeDataSet = BarChartDataSet()
-        val incomeValues = statEntries?.map(IncomeOutcomeEntry::income)
-        incomeDataSet.data = incomeValues
-        data.addChartDataSet(incomeDataSet)
-
-        val outcomeDataSet = BarChartDataSet()
-        val outcomeValues = statEntries?.map(IncomeOutcomeEntry::outcome)
-        outcomeDataSet.data = outcomeValues
-        data.addChartDataSet(outcomeDataSet)
-
-        barChart.data = data
-
-        return barChart
-    }
 }
